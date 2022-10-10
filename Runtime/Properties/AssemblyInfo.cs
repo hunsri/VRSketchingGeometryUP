da264b99743c06efc4695339073f526b6dd1d372
hunsri <richard.hunstock@gmx.de>
2022-10-10 19:23:40 +0200

    finished test integration

---
+++ Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+//Making internal Classes and Methods visible to the Tests 
+[assembly: InternalsVisibleTo("HTWBerlin.FKI.VRSketchingGeometry.Runtime.Tests")]
